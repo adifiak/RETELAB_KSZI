0a333ee9252e5f5222087dc759b243754ef15c0a
dantpu <daniel.tpu@gmail.com>
2024-03-18 19:37:56 +0100

    Test Jacococ3

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -48,7 +48,7 @@ public class TrainSensorTest {
         verify(mockedUser, times(0)).setAlarmState(true);
 		//Assert.assertEquals(false, user.getAlarmState());
 	}
-//asdasd
+//asdasdasd
     @Test
 	public void testnegativevalue() {
 		sensor.overrideSpeedLimit(-10);
