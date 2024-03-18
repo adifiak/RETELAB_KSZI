71815fb1704eec8446a55abfd5abfe5e237ecdc1
adifiak <adifiak@gmail.com>
2024-03-18 16:32:28 +0100

    Implemented reference speed refreshing.

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -1,5 +1,9 @@
 package hu.bme.mit.train.system;
 
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import hu.bme.mit.train.controller.TrainControllerImpl;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
@@ -12,6 +16,18 @@ public class TrainSystem {
 	private TrainController controller = new TrainControllerImpl();
 	private TrainUser user = new TrainUserImpl(controller);
 	private TrainSensor sensor = new TrainSensorImpl(controller, user);
+
+	TrainSystem(){
+		TimerTask task = new TimerTask() {
+			public void run() {
+				controller.followSpeed();
+			}
+		};
+		Timer timer = new Timer("Timer");
+    
+		long delay = 10L;
+		timer.schedule(task, delay);
+	}
 
 	public TrainController getController() {
 		return controller;
